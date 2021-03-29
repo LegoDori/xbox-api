69040718639c096059a6b6ce563d5a50e67578a3
Romanmender <romanmender3164@gmail.com>
2021-03-29 20:46:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,22 @@ exports.getprofilebygt = async function(gamertag, authorization) {
     return json
 }
 
-exports.getprofilebyxuid = async function(gamertag, options, authorization) {
+exports.getprofilebyxuid = async function(gamertag, authorization) {
     if(!gamertag) return 'null'
-    var response = await fetch('https://profile.xboxlive.com/users/xuid(' + gamertag + ')/profile/settings?settings=' + options , { method: 'GET', headers: {'x-xbl-contract-version': '2', 'Authorization': 'XBL3.0 x=' + authorization.userHash + ';' + authorization.XSTSToken }}).then(response => response.json())
-    return 
+    var fetched = await fetch('https://profile.xboxlive.com/users/xuid(' + gamertag + ')/profile/settings?settings=Gamertag,Gamerscore,GameDisplayPicRaw,AccountTier,XboxOneRep,PreferredColor,RealName,Bio,Location,ModernGamertag,RealNameOverride,Watermarks,IsQuarantined,DisplayedLinkedAccounts', { method: 'GET', headers: {'x-xbl-contract-version': '2', 'Authorization': 'XBL3.0 x=' + authorization.userHash + ';' + authorization.XSTSToken }}).then(response => response.json())
+    var json = {}
+    json.id = fetched.profileUsers[0].id
+    json.isSponsoredUser = fetched.profileUsers[0].isSponsoredUser
+    fetched.profileUsers[0].settings.forEach(settings => {if(settings.id != 'DisplayedLinkedAccounts') { json[settings.id] = settings.value } else { json[settings.id] = JSON.parse(settings.value) } });
+    return json
+}
+
+exports.getownprofile = async function(authorization) {
+    if(!gamertag) return 'null'
+    var fetched = await fetch('https://profile.xboxlive.com/users/me/profile/settings?settings=Gamertag,Gamerscore,GameDisplayPicRaw,AccountTier,XboxOneRep,PreferredColor,RealName,Bio,Location,ModernGamertag,RealNameOverride,Watermarks,IsQuarantined,DisplayedLinkedAccounts', { method: 'GET', headers: {'x-xbl-contract-version': '2', 'Authorization': 'XBL3.0 x=' + authorization.userHash + ';' + authorization.XSTSToken }}).then(response => response.json())
+    var json = {}
+    json.id = fetched.profileUsers[0].id
+    json.isSponsoredUser = fetched.profileUsers[0].isSponsoredUser
+    fetched.profileUsers[0].settings.forEach(settings => {if(settings.id != 'DisplayedLinkedAccounts') { json[settings.id] = settings.value } else { json[settings.id] = JSON.parse(settings.value) } });
+    return json
 }
