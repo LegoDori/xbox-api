22a736108316055da9b2346c531954f9959a8d84
Romanmender <romanmender3164@gmail.com>
2021-03-29 16:24:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 fetch = require('node-fetch')
 
+class Client {
+    constructor(hash, token) {
+        console.log(has + " " + token)
+    }
+}
+
 exports.random = function(gamertag, options) {
     if(!gamertag) return 'null'
-  return (Math.random() * (max-min)) + min;
+    return await fetch('https://profile.xboxlive.com/users/gt(' + gamertag + ')/profile/settings?settings=' + options , { method: 'GET', headers: {'x-xbl-contract-version': '2', 'Authorization': 'XBL3.0 x=' + xbox_token_final.DisplayClaims.xui[0].uhs + ";" + xbox_token_final.Token}}).then(response => response.json())
 }
