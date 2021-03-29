d70a40b16f2c8c5fce2759a23f4251ec994eee7c
Romanmender <romanmender3164@gmail.com>
2021-03-29 15:57:48 +0100

    ggg

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-function randomNoGenerator(min, max) {
-    if(typeof(max) !== 'number' && typeof(min) !== 'number') {
-      min = 0;  max = 1;
-    }
-   return (Math.random() * (max-min)) + min;
-  }
-  
-module.exports = randomNoGenerator;
+require('./src/profile')
