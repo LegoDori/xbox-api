cb0cde20f743de127d2ec06ca73249cdbebd9209
Romanmender <romanmender3164@gmail.com>
2021-03-29 16:00:48 +0100

    Delete profile.js

--- src/profile.js
+++
@@ -1,7 +0,0 @@
-exports.random = function(min, max) {
-   if(typeof(max) !== 'number' && typeof(min) !== 'number') {
-     min = 0;  max = 1;
-   }
-  return (Math.random() * (max-min)) + min;
-}
-  
