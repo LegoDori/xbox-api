cdfe26024dffeea9dbb970a8ff26efe7cd1737d1
Romanmender <romanmender3164@gmail.com>
2021-04-05 08:50:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ exports.getprofilexuid = async function(gamertag, authorization) {
 
 exports.getprofilescreenshots = async function(xuid, authorization) {
     try {
-    var fetched = await fetch('https://gameclipsmetadata.xboxlive.com/users/' + xuid + '/clips', { method: 'GET', headers: {'x-xbl-contract-version': '2', 'Authorization': 'XBL3.0 x=' + authorization.userHash + ';' + authorization.XSTSToken }}).then(response => response.json())
+    var fetched = await fetch('https://gameclipsmetadata.xboxlive.com/users/xuid(' + xuid + ')/clips', { method: 'GET', headers: {'x-xbl-contract-version': '2', 'Authorization': 'XBL3.0 x=' + authorization.userHash + ';' + authorization.XSTSToken }}).then(response => response.json())
     return fetched
     } catch {
         return 'null'
